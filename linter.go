845e7aefe3320bdef3c37d9853c0d13cd3a08951
alingse <alingse@foxmail.com>
2024-03-17 16:23:57 +0800

    remove comment

--- linter.go
+++ linter.go
@@ -53,53 +53,6 @@ func (a *analyzer) run(pass *analysis.Pass) (interface{}, error) {
 	return nil, nil
 }
 
-/*
-		if !isHTTPFuncOrMethodOnClient(pass.TypesInfo, call) {
-			return true // the function call is not related to this check.
-		}
-
-		// Find the innermost containing block, and get the list
-		// of statements starting with the one containing call.
-		stmts, ncalls := restOfBlock(stack)
-		if len(stmts) < 2 {
-			// The call to the http function is the last statement of the block.
-			return true
-		}
-
-		// Skip cases in which the call is wrapped by another (#52661).
-		// Example:  resp, err := checkError(http.Get(url))
-		if ncalls > 1 {
-			return true
-		}
-
-		asg, ok := stmts[0].(*ast.AssignStmt)
-		if !ok {
-			return true // the first statement is not assignment.
-		}
-
-		resp := rootIdent(asg.Lhs[0])
-		if resp == nil {
-			return true // could not find the http.Response in the assignment.
-		}
-
-		def, ok := stmts[1].(*ast.DeferStmt)
-		if !ok {
-			return true // the following statement is not a defer.
-		}
-		root := rootIdent(def.Call.Fun)
-		if root == nil {
-			return true // could not find the receiver of the defer call.
-		}
-
-		if resp.Obj == root.Obj {
-			pass.ReportRangef(root, "using %s before checking for errors", resp.Name)
-		}
-		return true
-	})
-
-inspect.WithStack(nodeFilter, func(n ast.Node, push bool, stack []ast.Node) bool {
-*/
-
 func (a *analyzer) process(pass *analysis.Pass, n ast.Node, push bool, stack []ast.Node) {
 	if !push {
 		return
