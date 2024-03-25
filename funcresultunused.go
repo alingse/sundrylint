8ce45ae02b564f90ff08686aa03e08310c728f90
alingse <alingse@foxmail.com>
2024-03-26 00:50:48 +0800

    add it

--- funcresultunused.go
+++ funcresultunused.go
@@ -21,7 +21,7 @@ func LintFuncResultUnused(pass *analysis.Pass, node *ast.CallExpr, stack []ast.N
 	if sign.Results().Len() == 0 {
 		return nil
 	}
-	if !IsTupleAll(sign.Params(), IsBasicType) {
+	if !IsTupleAll(sign.Params(), IsBasicType) || !IsTupleAll(sign.Results(), IsBasicType) {
 		return nil
 	}
 	funcName, err := GetCode(pass.Fset, node.Fun)
