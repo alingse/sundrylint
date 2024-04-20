2db676ef9fcd73f467f5570b0e462b9b1079e727
alingse <alingse@foxmail.com>
2024-04-20 23:30:12 +0800

    add tmp

--- .github/submit.sh
+++ .github/submit.sh
@@ -1,4 +1,4 @@
 #!bin/bash
 echo $1
-gh search repos $1 --limit 100 --language=go --json url --jq '.[]|.url' | xargs -I {} gh workflow run .github/workflows/check-any.yaml -F repo_url={} -F linter_url=github.com/ashanbrown/makezero@latest -F linter_name=makezero
+gh search repos $1 --limit 100 --language=go --json url --jq '.[]|.url' | xargs -I {} gh workflow run .github/workflows/check-any.yaml -F repo_url={} -F linter_url=github.com/alingse/makezero@rename-go-mod -F linter_name=makezero
 
