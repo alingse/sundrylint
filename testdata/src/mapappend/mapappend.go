0aa8e5199b82fdb873ee108f9591d4ea209d4172
alingse <alingse@foxmail.com>
2025-05-29 22:47:55 +0800

    add another case

--- testdata/src/mapappend/mapappend.go
+++ testdata/src/mapappend/mapappend.go
@@ -9,3 +9,15 @@ func goodAppend() {
 	m := make(map[string][]int)
 	m["a"] = append(m["a"], 1) // 正确的用法，key相同
 }
+
+type TimeType int
+
+const (
+	Month TimeType = 1
+	Day   TimeType = 1
+)
+
+func badAppend2() {
+	m := make(map[TimeType][]int)
+	m[Month] = append(m[Day], 1) // want `potential incorrect map append: using different keys for append and assignment`
+}
